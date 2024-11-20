8791c2bc764dd06e93fb5d2435c434ff81f9f048
Brooke Bryan <brooke@bajb.net>
2024-11-20 13:14:29 +0000

    Card Brand & Networks

---
+++ brand.go
@@ -0,0 +1,14 @@
+package cht
+
+type Brand string
+
+const (
+	BrandVisa            Brand = "visa"
+	BrandMastercard      Brand = "mastercard"
+	BrandAmex            Brand = "amex"
+	BrandDiscover        Brand = "discover"
+	BrandDiners          Brand = "diners"
+	BrandJCB             Brand = "jcb"
+	BrandUnionPay        Brand = "unionpay"
+	BrandCartesBancaires Brand = "cartesbancaires"
+)
---
+++ network.go
@@ -0,0 +1,30 @@
+package cht
+
+type Network string
+
+const (
+	NetworkAccel         Network = "accel"
+	NetworkAmex          Network = "amex"
+	NetworkBancontact    Network = "bancontact"
+	NetworkCarteBancaire Network = "cartebancaire"
+	NetworkCuliance      Network = "culiance"
+	NetworkDankort       Network = "dankort"
+	NetworkDinersClub    Network = "dinersclub"
+	NetworkDiscover      Network = "discover"
+	NetworkEftpos        Network = "eftpos"
+	NetworkElo           Network = "elo"
+	NetworkHipercard     Network = "hipercard"
+	NetworkJcb           Network = "jcb"
+	NetworkKoreanLocal   Network = "koreanlocal"
+	NetworkMastercard    Network = "mastercard"
+	NetworkMir           Network = "mir"
+	NetworkNyce          Network = "nyce"
+	NetworkPrivateLabel  Network = "privatelabel"
+	NetworkProprietary   Network = "prop"
+	NetworkPulse         Network = "pulse"
+	NetworkRupay         Network = "rupay"
+	NetworkStar          Network = "star"
+	NetworkUatp          Network = "uatp"
+	NetworkUnionpay      Network = "unionpay"
+	NetworkVisa          Network = "visa"
+)
